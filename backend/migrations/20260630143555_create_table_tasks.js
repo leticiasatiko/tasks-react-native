2361f58e6fbc6a5f5310f152a77284633ab81a52
Leticia Takahashi <leticiasatiko@gmail.com>
2026-06-30 17:07:42 -0300

    backend

---
+++ backend/migrations/20260630143555_create_table_tasks.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('tasks', function(table) {
+        table.increments('id').primary();
+        table.string('desc').notNull();
+        table.datetime('estimateAt');
+        table.datetime('doneAt');
+        table.integer('userId').references('id').inTable('users').notNull();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('tasks');
+};
